cf6e10812a5ad1d35fb304a3a159782a639be0ad
Shahadat Shuvo <shuvombm02@gmail.com>
2017-04-29 03:52:52 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     
+		$( "#slider-range" ).slider({
+		  range: true,
+		  min: 0,
+		  max: 500,
+		  values: [ 50, 400 ],
+		  slide: function( event, ui ) {
+			$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+		  }
+		});
+		$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+		  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+        
+	  });
+
+  
+
+
+}(jQuery)); 
